4d9f3657e2c72977cad0f774e1e370ac66ce15d5
yifanhao <yifanhao@students.cs.ubc.ca>
2023-04-02 12:18:59 -0400

    flip command

---
+++ src/commands/Flip.ts
@@ -0,0 +1,31 @@
+import { Client, CommandInteraction, ApplicationCommandType, AttachmentBuilder, EmbedBuilder } from "discord.js";
+import { Command } from "../Command";
+
+/*
+    export interface ChatInputApplicationCommandData extends BaseApplicationCommandData {
+        description: string;
+        descriptionLocalizations?: LocalizationMap;
+        type?: ApplicationCommandType.ChatInput;
+        options?: ApplicationCommandOptionData[];
+    }
+*/
+
+export const Flip: Command = {
+    name: "flip",
+    description: "flips a coin",
+    type: ApplicationCommandType.ChatInput,
+    run: async (BOT: Client, interaction: CommandInteraction) => {
+        const condition: Boolean = Math.random() < 0.5;
+
+        const headsImage: AttachmentBuilder = new AttachmentBuilder('./src/assets/images/hub.png');
+        const tailsImage: AttachmentBuilder = new AttachmentBuilder('./src/assets/images/terry_schleep.jpeg');
+
+        const embed: EmbedBuilder = new EmbedBuilder().setTitle(condition ? 'Heads' : 'Tails')
+            .setImage(condition ? 'attachment://hub.png' : 'attachment://terry_schleep.jpeg');
+
+        await interaction.followUp({
+            embeds: [embed],
+            files: condition ? [headsImage] : [tailsImage]
+        });
+    }
+}
