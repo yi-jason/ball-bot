d10c08646c6eb76b2fd6f66793dda12683d09376
Jason <jason2580134@outlook.com>
2023-04-02 21:11:50 -0700

    Include new files

---
+++ src/commands/Gif.ts
@@ -0,0 +1,40 @@
+import { Client, CommandInteraction, ApplicationCommandType, ApplicationCommandOptionType, ApplicationCommandOptionData } from "discord.js";
+import { getDatabase, ref, set } from "firebase/database";
+import { Command } from "../Command";
+
+/*
+    export interface ChatInputApplicationCommandData extends BaseApplicationCommandData {
+        description: string;
+        descriptionLocalizations?: LocalizationMap;
+        type?: ApplicationCommandType.ChatInput;
+        options?: ApplicationCommandOptionData[];
+    }
+*/
+
+const gifOptionDefault: string = "name";
+
+const gifCommandOptions: ApplicationCommandOptionData[] = [
+    {
+        name: gifOptionDefault,
+        description: "Name of the gif",
+        type: ApplicationCommandOptionType.String,
+        required: true,
+    }
+]
+
+export const Gif: Command = {
+    name: "g",
+    description: "Sends a gif with the saved name",
+    type: ApplicationCommandType.ChatInput,
+    options: gifCommandOptions,
+    run: async (BOT: Client, interaction: CommandInteraction) => {
+        const gif = interaction.options.get(gifOptionDefault, true).value?.toString();
+
+        const db = getDatabase();
+        const reference = ref(db, 'gif-list/' + gif);
+
+        set(reference, {
+            url: "Test"
+        });
+    }
+}
---
+++ src/config/configFireBase.ts
@@ -0,0 +1,15 @@
+import dotenv from "dotenv"
+dotenv.config();
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCbTLAL-8PuUnH3rhZkO7y4FBGpNE4k__8",
+    authDomain: "ball-bot-40285.firebaseapp.com",
+    databaseURL: "https://ball-bot-40285-default-rtdb.firebaseio.com",
+    projectId: "ball-bot-40285",
+    storageBucket: "ball-bot-40285.appspot.com",
+    messagingSenderId: "647674860309",
+    appId: "1:647674860309:web:85aa39daafb257fd3e0f1c",
+    measurementId: "G-74TTYLB6VN"
+};
+
+export default firebaseConfig
